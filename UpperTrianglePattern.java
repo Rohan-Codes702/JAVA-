df929a17a47eb4afb4c9b2edc586cf58927c7fed
Rohan-Codes702 <rohanmane702@gmail.com>
2024-11-02 21:32:03 +0530

    Add files via upload

---
+++ UpperTrianglePattern.java
@@ -0,0 +1,25 @@
+package BasicJava.java;
+
+public class UpperTrianglePattern {
+    public static void size(int n)
+    {
+        for (int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=i;j++)
+            {
+                System.out.print(" ");
+            }
+            for (int j=i;j<=n;j++)
+            {
+                System.out.print("*"+" ");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int n=7;
+         size(n);
+    }
+}
